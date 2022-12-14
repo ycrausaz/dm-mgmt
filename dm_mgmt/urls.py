e5fc7e4aa3391a050ccbe2ee65ffb6a999211a10
ycrausaz <yann.crausaz@gmail.com>
2022-12-14 16:11:26 +0100

    Updated (in comment) the urls so that they follow CBV 'rules'

--- dm_mgmt/urls.py
+++ dm_mgmt/urls.py
@@ -29,6 +29,19 @@ urlpatterns = [
     path('output_service_pdf', views.output_service_pdf, name='output-service-pdf'),
     path('login_user', views.login_user, name='login-user'),
     path('logout_user', views.logout_user, name='logout-user'),
-#    path('tbd', views.tbd, name='tbd'),
-#    path('jsi18n', JavaScriptCatalog.as_view(), name='javascript-catalog'),
+
+#    path('', views.home, name='home'),
+#
+#    path('services/create/', login_required(AddServiceView.as_view()), name='add-service'),
+#    path('services/<int:pk>/update', login_required(UpdateServiceView.as_view()), name='update-service'),
+#    path('services/<int:pk>/show', login_required(ShowServiceView.as_view()), name='show-service'),
+#    path('services/<int:pk>/delete', login_required(DeleteServiceView.as_view()), name='delete-service'),
+#    path('services/list', login_required(ListServicesView.as_view()), name='list-services'),
+#
+#    path('clients/create/', login_required(AddClientView.as_view()), name='add-client'),
+#    path('clients/<int:pk>/update', login_required(UpdateClientView.as_view()), name='update-client'),
+#    path('clients/<int:pk>/show', login_required(ShowClientView.as_view()), name='show-client'),
+#    path('clients/list', login_required(ListClientsView.as_view()), name='list-clients'),
+#
+#    path('massages/list', login_required(ListMassagesView.as_view()), name='list-massages'),
 ]
